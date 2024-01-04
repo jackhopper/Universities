3d861c954a05b61f2d9c48b6c6fc9be6f620d50e
jackhopper <76411205+jackhopper@users.noreply.github.com>
2024-01-03 21:38:39 -0600

    Add title and formatting

--- app.R
+++ app.R
@@ -11,18 +11,23 @@ schools <- read_csv('https://raw.githubusercontent.com/jackhopper/Universities/m
 
 #Define UI for application
 ui <- shinyUI(fluidPage(
-  
+  h1("University K-Means Clustering"),
+  h2("Choose your own clusters & view your results"),
+  h4("K-Means clustering groups data across 6 variables to provide quantitative measures of similarity"),
   # Define side panel with slider input
   sidebarLayout(
     sidebarPanel(
-      sliderInput("num_clusters", "Number of clusters:", min = 1, max = 10, value = 3)
+      sliderInput("num_clusters", "Number of clusters:", min = 1, max = 10, value = 3, step = 1,
+                  helpText("Adjust the number of clusters to explore different groupings."))
     ),
     
     # Main panel to display clustering results
     mainPanel(
       tabsetPanel(
-        tabPanel("PCA Analysis", plotlyOutput("firstPlot")),
-        tabPanel("Scatterplot with Clusters", plotlyOutput("secondPlot"))
+        tabPanel("PCA Analysis", plotlyOutput("firstPlot"),
+                 p("This graph shows the clustering results across all 6 dimensions reduced down to two key 'principal components'.")),
+        tabPanel("Scatterplot with Clusters", plotlyOutput("secondPlot"),
+                 p("This graph shows the clusters overlaid on two key dimensions of the analysis: ACT score and average tuition."))
       )
     )
   )
@@ -43,7 +48,7 @@ server <- shinyServer(function(input, output) {
   # Apply cluster assignments to raw data
   data_with_clusters <- reactive({
     schools %>% 
-      mutate(cluster = clusters()$cluster) 
+      mutate(cluster = as.factor(clusters()$cluster)) 
   })
   
   # Generate the raw fviz cluster plot
